138e02bb54611d265b5d49895549ac72d53efd1e
Siddarth Vijay <vsiddarthvijay@gmail.com>
2020-09-03 23:24:25 +0530

    test constructor

--- contracts/Auction.sol
+++ contracts/Auction.sol
@@ -13,7 +13,7 @@ contract Auction {
     mapping(address => uint256) public escrow;
 
     // constructor(address payable _barbossa) public {
-    constructor() {
+    constructor() public {
         biddingClosed = false;
         auctionClosed = false;
         barbossa = 0x89205A3A3b2A69De6Dbf7f01ED13B2108B2c43e7;
