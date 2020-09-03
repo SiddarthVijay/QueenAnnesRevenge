fedc0cc5851fa24433483955d1741020690f5487
Siddarth Vijay <vsiddarthvijay@gmail.com>
2020-09-03 21:39:01 +0530

    finished commit and reveal funcs

--- contracts/Auction.sol
+++ contracts/Auction.sol
@@ -2,17 +2,30 @@ pragma solidity >=0.4.22 <0.8.0;
 
 contract Auction {
     bool public biddingClosed;
-    bool public AuctionClosed;
+    bool public auctionClosed;
     // uint256 public bidPeriodEndTime;
     // uint256 public auctionEndTime;
-    address public beneficiary;
+    address public barbossa;
 
-    constructor(address _beneficiary) public {
+    mapping(address => uint256) public hashedEscrow;
+    mapping(address => uint256) public escrow;
+
+    constructor(address _barbossa) public {
         biddingClosed = false;
         auctionClosed = false;
+        barbossa = _barbossa;
     }
 
-    function commitBid(uint256 bid) external {
+    function commitBid(uint256 commit) external {
         require(!biddingClosed);
+
+        hashedEscrow[msg.sender] = commit;
+    }
+
+    function revealBid(uint256 nonce) external payable {
+        require(biddingClosed);
+        // require(msg.value == dehash(nonce, hashedEscrow[msg.sender]));
+
+        escrow[msg.sender] = msg.value;
     }
 }
